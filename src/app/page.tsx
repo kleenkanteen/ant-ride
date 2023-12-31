443435638cbbf19b551768a827187323c84304b6
kleenkanteen <antpalmolive@gmail.com>
2023-12-31 03:02:47 -0500

    partially done create carpool page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] my-4">
-          ant ride
-        </h1>
+    <div className="flex flex-col border-red-500 border-2 items-center gap-12">
+      <h1 className="text-5xl font-extrabold tracking-tight text-white	 sm:text-[5rem] my-4">
+        The
+        {' '}
+        <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+          easiest
+        </span>
+        {' '}
+        way to organize carpools.
+      </h1>
+      <div className="flex flex-col gap-4 w-96 justify-center border-red-500 border-2">
         <Link href="/create" className="btn btn-outline btn-lg">Create carpool</Link>
         <Link href="/join" className="btn btn-outline btn-lg">Join carpool</Link>
         <Link href="/edit-carpool" className="btn btn-outline btn-lg">Edit carpool as organizer</Link>
         <Link href="/edit-join" className="btn btn-outline btn-lg">Edit carpool as participant</Link>
-      </div>      
-    </main>
+      </div>
+    </div>
   );
 }
