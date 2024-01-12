6558721a63f8ba18e9456c5fb75550ee29403284
kleenkanteen <antpalmolive@gmail.com>
2024-01-11 20:27:20 -0500

    forgot to do in previous commit, reuse component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
-
+import { CarpoolDetails } from '@/components/carpoolDetails';
 
 interface IFormInputs {
     event_name: string,
@@ -54,67 +54,7 @@ export default function Create() {
         <ThemeProvider theme={darkTheme}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex flex-col gap-4">
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Event name: </span>
-                            </div>
-                            <input type="text" placeholder="Name..." max="40" className="input input-bordered w-full max-w-xs"
-                                {...register("event_name")} />
-                                {errors.event_name?.message && <br />}
-                                <p className="text-red-500">{errors.event_name?.message}</p>
-                        </label>
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Location: </span>
-                            </div>
-                            <input type="text" placeholder="Paste the full address: " max="40" className="input input-bordered w-full max-w-xs"
-                                {...register("location")} />
-                                {errors.location?.message && <br />}
-                                <p className="text-red-500">{errors.location?.message}</p>
-                        </label>
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Date: </span>
-                            </div>
-                            <Controller
-                                name="date"
-                                control={control}
-                                render={({ field }) => (
-                                    <MobileDatePicker
-                                        sx={{ width: "100% " }}
-                                        {...field}
-                                        slotProps={{
-                                            textField: {
-                                                helperText: errors.date ? "Date is a required field" : '',
-                                            },
-                                        }}
-                                    />
-                                )}
-                            />
-                        </label>
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Time: </span>
-                            </div>
-                            <Controller
-                                name="time"
-                                control={control}
-                                render={({ field }) => (
-                                    <MobileTimePicker
-                                        sx={{ width: "100% " }}
-                                        {...field}
-                                        slotProps={{
-                                            textField: {
-                                                helperText: errors.time ? "Time is a required field" : '',
-                                            },
-                                        }}
-                                    />
-                                )}
-                            />
-                        </label>
-                        <button className="btn btn-outline btn-md my-4">Submit</button>
-                    </div>
+                    <CarpoolDetails register={register} errors={errors} control={control} onSubmit={onSubmit} />
                 </form>
             </LocalizationProvider>
         </ThemeProvider>
