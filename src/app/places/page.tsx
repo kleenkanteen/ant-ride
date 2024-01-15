7a05f754063261982ba5054da6fd8a989277ade3
kleenkanteen <antpalmolive@gmail.com>
2024-01-14 23:22:17 -0500

    fixed issue

--- src/app/places/page.tsx
+++ src/app/places/page.tsx
@@ -7,110 +7,100 @@
 "use client"
 
 import { Loader } from '@googlemaps/js-api-loader';
-import './App.css';
 import PlacesAutocomplete, {
-    geocodeByAddress,
-    geocodeByPlaceId,
-    getLatLng,
-  } from 'react-places-autocomplete';
+  geocodeByAddress,
+  getLatLng
+} from 'react-places-autocomplete';
+import './App.css';
 
-  import React, {useState, useEffect} from 'react';
+import { useEffect, useState } from 'react';
 
-  const googleMapsApiKey = 'AIzaSyBWB6oii8RWedqCde4UE5Ou-bPBZGzv3mk';
+const googleMapsApiKey = 'AIzaSyBZdmJgO3NVSuKMDNRAnreRh5Hw8SnHotM';
 
 
 function Places() {
 
 
-const [address, setAddress]= useState("")
-const [coordinates, setCoordinates]= useState({
-    lat:null,
-    long:null
-})
+  const [address, setAddress] = useState("")
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    long: null
+  })
 
-const [apiLoaded, setApiLoaded] = useState(false);
+  const [apiLoaded, setApiLoaded] = useState(false);
 
 
-useEffect(() => {
-  const loader = new Loader({
-    apiKey: googleMapsApiKey,
-    version: "weekly",
-    libraries: ['places'],
-  });
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: googleMapsApiKey,
+      version: "weekly",
+      libraries: ['places'],
+    });
 
-  loader.load().then(() => {
-    console.log('Google Maps JavaScript API has been loaded');
-    setApiLoaded(true);
-  });
-}, []);
+    loader.load().then(() => {
+      console.log('Google Maps JavaScript API has been loaded');
+      setApiLoaded(true);
+    });
+  }, []);
 
   const handleSelect = async value => {
     const results = await geocodeByAddress(value);
-    
-    const ll= await getLatLng(results[0])
+
+    const ll = await getLatLng(results[0])
     console.log(ll)
     setAddress(value)
     setCoordinates(ll)
-}
+  }
 
-    /*return(
-        <div className='App'>
 
-<p> lat: {coordinates.lat} </p>
-<p> long: {coordinates.long} </p>
-<p> Address: {address} </p>*/
-      {/* <PlacesAutocomplete
-        value={address}
-        onChange={setAddress}
-        onSelect={handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div      
-          
-          >
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
+  return (
+    <div>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZdmJgO3NVSuKMDNRAnreRh5Hw8SnHotM&libraries=places"></script>
+      {apiLoaded &&
+        <PlacesAutocomplete
+          value={address}
+          onChange={setAddress}
+          onSelect={handleSelect}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div
+
+            >
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion, index) => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div key={index}
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span key={index}>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )}
-      </PlacesAutocomplete> */}
-     /*   </div>
-    )
-    */
+          )}
+        </PlacesAutocomplete>
+      }
 
-    return (
-      
-      <div>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWB6oii8RWedqCde4UE5Ou-bPBZGzv3mk&libraries=places"></script>
-        {apiLoaded && <PlacesAutocomplete onSelect={handleSelect} />}
-        
-      </div>
-     );
+    </div>
+  );
 
 }
 
