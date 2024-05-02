07cb190ad73f5d732d658546fbbc8d1d8a5ddbf4
kleenkanteen <antpalmolive@gmail.com>
2024-05-02 00:00:15 -0400

    gender field

--- src/app/edit-participant/page.tsx
+++ src/app/edit-participant/page.tsx
@@ -11,6 +11,7 @@ interface IParticipantDetails {
     edit_code: string,
     remove: boolean,
     name: string,
+    gender: 'Male' | 'Female',
     location: string,
     can_pickup: boolean,
     seats_available: number,
@@ -21,6 +22,7 @@ const schema = yup.object().shape({
     edit_code: yup.string().min(6).max(6).matches(/^[a-zA-Z0-9]+$/, "Invalid character(s) entered").required(),
     remove: yup.boolean().required(),
     name: yup.string().max(40).required(),
+    gender: yup.mixed().oneOf(['Male', 'Female']).required(),
     location: yup.string().max(40).required(),
     can_pickup: yup.boolean().required(),
     seats_available: yup.number().max(20).required()
--- src/components/participantDetails.tsx
+++ src/components/participantDetails.tsx
@@ -12,6 +12,28 @@ export function ParticipantDetails({ register, errors, onSubmit }) {
                 {errors.name?.message && <br />}
                 <p className="text-red-500">{errors.name?.message}</p>
             </label>
+            <div className="flex flex-row">
+            
+            </div>
+            <div>
+                <span className="label-text font-bold">Gender: </span>
+            </div>
+            <div>
+                <div className="form-control">
+                    <label className="label cursor-pointer">
+                        <span className="label-text">Male</span>
+                        <input type="radio" name="radio-10" className="radio checked:bg-blue-400"
+                        {...register("gender")}/>
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label cursor-pointer">
+                        <span className="label-text">Female</span>
+                        <input type="radio" name="radio-10" className="radio checked:bg-rose-300"
+                        {...register("gender")}/>
+                    </label>
+                </div>
+            </div>
             <label className="form-control w-full max-w-xs">
                 <div className="label">
                     <span className="label-text">Location: </span>
