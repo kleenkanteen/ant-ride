a2958b7b8c130585fe9d676264eb220b5e95ee7b
kleenkanteen <antpalmolive@gmail.com>
2024-05-26 23:32:28 -0400

    fix: ssr build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import { Suspense } from "react"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,7 +29,7 @@ export default function RootLayout({
             <h1 className="underline decoration-wavy decoration-blue-700 underline-offset-8 mb-4 text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
               ant ride
             </h1>
-            {children}
+            <Suspense>{children}</Suspense>
             <span className="font-bold text-white text-lg">Under construction</span>
             <span><a target="_blank" href="https://github.com/kleenkanteen/ant-ride">Made with ❤️ by Sabih & co.</a></span>
             <Toaster
