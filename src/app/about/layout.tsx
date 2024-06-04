ef521cfd59cad67fb61d65cc11c8cc974e6c1bee
Yohance Williams-Rodriguez <balamyohance@gmail.com>
2024-06-04 12:41:19 -0500

    fix: fixed /carpools?event-code error

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="lg:px-[20%]">{children}</div>;
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <>
+      <p>
+        Ant ride is a carpooling website to make it as easy and efficient as
+        possible to organize carpools. What makes it different from existing
+        websites is that it automatches all participants into carpools 24 hours
+        before the event start. This eliminates the coordination usually needed
+        where everyone has to choose a car to ride in. It also allows the
+        creation of the most optimal carpool routes based on everyone’s
+        location.
+      </p>
+    </>
+  );
+}
