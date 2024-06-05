1d0e886d1d455535d1ad7e6ff8c4c208a590a36e
kleenkanteen <antpalmolive@gmail.com>
2024-06-05 18:15:23 -0400

    chore: minor frontend touchups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center items-center gap-12">
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] my-4 mx-2 text-center">
+      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] mx-2 text-center">
         The
         {' '}
         <span className="bg-gradient-to-r from-indigo-700 via-blue-500 to-green-600 inline-block text-transparent bg-clip-text">
