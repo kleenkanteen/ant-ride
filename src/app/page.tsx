fd328d675707623fe8a244e4696422ff8ebe85c2
kleenkanteen <antpalmolive@gmail.com>
2024-09-26 09:51:11 -0400

    touchups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center items-center gap-12">
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] mx-2 text-center">
-        The
-        {' '}
-        <span className="bg-gradient-to-r from-indigo-700 via-blue-500 to-green-600 inline-block text-transparent bg-clip-text">
-          easiest
-        </span>
-        {' '}
-        way to organize carpools.
-      </h1>
+      <div style={{ maxWidth: '90%' }}>
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] mx-2 text-center">
+          The
+          {' '}
+          <span className="bg-gradient-to-r from-indigo-700 via-blue-500 to-green-600 inline-block text-transparent bg-clip-text">
+            easiest
+          </span>
+          {' '}
+          way to organize carpools.
+        </h1>
+      </div>
       <div className="flex flex-row justify-center" style={{ maxWidth: '80%' }}>
         <div className="flex flex-col gap-4 w-96 justify-center">
           <Link href="/join" className="btn btn-outline btn-lg">Join event</Link>
